e1b6fba62c7ac89a61be7e0fe9c89973b2946d98
John Watson <john.watson1@defra.gov.uk>
2024-09-27 20:26:06 +0100

    Add blipp

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,10 +1,12 @@
 import { Server } from '@hapi/hapi'
 import Inert from '@hapi/inert'
 import Crumb from '@hapi/crumb'
+import Blipp from 'blipp'
 import logging from './logging.js'
 import errors from './errors.js'
 import views from './views.js'
 import router from './router.js'
+import config from '../config.js'
 
 async function registerPlugins (server: Server): Promise<void> {
   const plugins: any[] = [
@@ -15,6 +17,10 @@ async function registerPlugins (server: Server): Promise<void> {
     views,
     router,
   ]
+
+  if (config.get('isDev')) {
+    plugins.push(Blipp)
+  }
 
   await server.register(plugins)
 }
