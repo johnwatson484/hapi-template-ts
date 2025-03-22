8e7453ba8d830c9035007fe6ecc4598f3b681291
John Watson <john.watson1@defra.gov.uk>
2025-03-22 13:31:32 +0000

    Add CSP

--- src/ts/plugins/index.ts
+++ src/ts/plugins/index.ts
@@ -1,6 +1,9 @@
 import { Server } from '@hapi/hapi'
 import Inert from '@hapi/inert'
 import Crumb from '@hapi/crumb'
+import Scooter from '@hapi/scooter'
+import csp from './content-security-policy.js'
+import headers from './headers.js'
 import logging from './logging.js'
 import errors from './errors.js'
 import views from './views.js'
@@ -11,8 +14,11 @@ async function registerPlugins (server: Server): Promise<void> {
   const plugins: any[] = [
     Inert,
     Crumb,
+    Scooter,
+    csp,
     logging,
     errors,
+    headers,
     views,
     router,
   ]
